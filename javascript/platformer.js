534059da7083c6a37eb8d70e29b9756bbf4ecf16
SteveMan67 <poppt@acatulsa.org>
2026-01-20 20:12:21 -0600

    editor image placement and movement

--- javascript/platformer.js
+++ javascript/platformer.js
@@ -1,8 +1,13 @@
 const canvas = document.querySelector("canvas")
+const dpr = window.devicePixelRatio
 const ctx = canvas.getContext('2d')
 const rect = canvas.getBoundingClientRect()
 canvas.width = rect.width
 canvas.height = rect.height
+
+ctx.setTransform(dpr, 0, 0, dpr, 0, 0)
+ctx.imageSmoothingEnabled = false
+canvas.style.imageRendering = 'pixelated'
 
 function decodeRLE(rle) {
   const out = []
@@ -51,6 +56,118 @@ function createMap(width, height, data) {
   return json
 }
 
-function main() {
+function loadTileset(manifestPath) {
+  return fetch(manifestPath)
+    .then(res => res.json())
+    .then(manifest => {
+      const promises = manifest.files.map(filename => {
+        return new Promise((resolve, reject) => {
+          const img = new Image()
+          img.src = manifest.path + filename
+          img.onload = () => resolve(img)
+          img.onerror = reject
+        })
+      })
+
+      return Promise.all(promises)
+        .then(images => [null, ...images])
+    })
+}
+
+function platformerLoop() {
 
 }
+
+const editor = {
+  cam: {
+    x: 0,
+    y: 0
+  },
+  tileSize: 16,
+  selectedTile: 1,
+  map: null,
+  tileset: []
+}
+
+const input = {
+  x: 0,
+  y: 0,
+  down: false,
+  keys: {}
+}
+
+function initEditor() {
+  window.addEventListener('keydown', e => input.keys[e.key] = true)
+  window.addEventListener('keyup', e => input.keys[e.key] = false)
+
+  canvas.addEventListener('mousemove', e => {
+    const rect = canvas.getBoundingClientRect()
+    input.x = e.clientX - rect.left
+    input.y = e.clientY - rect.top
+  })
+  canvas.addEventListener('mousedown', () => input.down = true)
+  canvas.addEventListener('mouseup', () => input.down = false)
+
+  loadTileset('assets/tileset.json').then(images => {
+    editor.tileset = images
+    editor.map = {
+      w: 100, h: 50, tiles: new Uint16Array(100 * 50)
+    }
+    levelEditorLoop()
+  })
+}
+
+function levelEditorLoop() {
+  const { map, cam, tileSize, tileset} = editor
+
+  const speed = 5
+  if (input.keys['w']) cam.y -= speed
+  if (input.keys['s']) cam.y += speed
+  if (input.keys['a']) cam.x -= speed
+  if (input.keys['d']) cam.x += speed
+
+  const worldX = input.x + cam.x
+  const worldY = input.y + cam.y
+  const tx = Math.floor(worldX / tileSize)
+  const ty = Math.floor(worldY / tileSize)
+
+  if (input.down) {
+     if (tx >= 0 && tx < map.w && ty >= 0 && ty < map.h) {
+      const idx = ty * map.w + tx
+      map.tiles[idx] = editor.selectedTile
+     }
+  }
+  
+  ctx.fillStyle = '#C29A62'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  const startX = Math.floor(cam.x / tileSize)
+  const endX = startX + (canvas.width / tileSize)
+  const startY = Math.floor(cam.y / tileSize)
+  const endY = startY + (canvas.width / tileSize)
+  
+  for (let y = startY; y < endY; y++) {
+    for (let x = startX; x < endX; x++) {
+      if (x < 0 || x >= map.w || y < 0 || y >= map.h) continue
+      const tileId = map.tiles[y * map.w + x]
+      const scrX = (x * tileSize) - cam.x
+      const scrY = (y * tileSize) - cam.y
+      
+      if (tileId > 0 && tileset[tileId]) {
+        ctx.drawImage(tileset[tileId], scrX, scrY, tileSize, tileSize)
+      } else {
+        ctx.strokeStyle = 'grey'
+        ctx.strokeRect(scrX, scrY, tileSize, tileSize)
+      }
+    }
+  }
+
+  const cursorScrX = (tx * tileSize) - cam.x
+  const cursorScrY = (ty * tileSize) - cam.y
+  ctx.strokeStyle = 'grey'
+  ctx.strokeRect(cursorScrX, cursorScrY, tileSize, tileSize)
+
+  requestAnimationFrame(levelEditorLoop)
+}
+
+initEditor()
