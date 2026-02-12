48ffe2062280e254932ac3eae8094d62475e7d7a
SteveMan67 <SteveMan67@users.noreply.github.com>
2026-02-12 13:01:55 +0000

    deploy: ef06d934999fa6ae7753f2c87ea7c542d51df9a6

--- javascript/level.js
+++ javascript/level.js
@@ -1,9 +1,12 @@
 const serverUrl = "http://localhost:1010"
 async function getLevel(level = 1) {
   try {
-    const levels = await fetch(`${serverUrl}/api/level?levelId=${level}`)
-    window.dispatchEvent(new CustomEvent('level:loaded', { detail: data }))
-    return levels.json()
+    const raw = await fetch(`${serverUrl}/api/level?levelId=${level}`)
+    const levels = raw.json()
+    console.log(levels)
+
+    window.dispatchEvent(new CustomEvent('level:loaded', { detail: levels }))
+    return await levels
   } catch (e) {
     console.error(e)
   }
@@ -19,14 +22,15 @@ const finishes = document.querySelector(".finishes")
 const levelNum = Number(window.location.href.match(/\/level\/(\d+)/)[1])
 
 getLevel(levelNum).then(level => {
+  console.log(level)
   if (!level) {
     window.location.href = "/"
   } else {
     console.log(level[0])
-    levelName.innerHTML = level[0].name
-    approvalPercentage.innerHTML = `${level[0].approval_percentage}%`
-    description.innerHTML = level[0].description
-    plays.innerHTML = level[0].total_plays
-    finishes.innerHTML = level[0].finished_plays
+    levelName.innerHTML = level.name
+    approvalPercentage.innerHTML = `${level.approval_percentage}%`
+    description.innerHTML = level.description
+    plays.innerHTML = level.total_plays
+    finishes.innerHTML = level.finished_plays
   }
 })
